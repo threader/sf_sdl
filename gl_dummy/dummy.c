344c1ef6a44b724df1ed53a6c3cf4dbbc52dfabc
bernd_afa <bernd_afa@d67ab018-90f1-4466-8658-4518960f82dd>
2009-02-19 18:05:40 +0000

    a libgl_dummy to use same sdl lib with and without opengl progs and if a program not need opengl then can link with libgl_dummy and stormmesa is not load

---
+++ gl_dummy/dummy.c
@@ -0,0 +1,242 @@
+void * AmigaMesaGetConfig()
+{
+ return 0; 
+}
+
+void * AmigaMesaCreateContext()
+{
+ return 0; 
+}
+
+void * AmigaMesaDestroyContext()
+{
+ return 0; 
+}
+
+void * AmigaMesaSetRast()
+{
+ return 0; 
+}
+
+void * AmigaMesaMakeCurrent()
+{
+ return 0; 
+}
+void * AmigaMesaSwapBuffers()
+{
+ return 0; 
+}
+void glClearIndex(){}
+void glClearColor(){}
+void glPushAttrib(){}
+void glHint(){}
+void glClear(){}
+void glIndexMask(){}
+void glColorMask(){}
+void glAlphaFunc(){}
+void glBlendFunc(){}
+void glLogicOp(){}
+void glCullFace(){}
+void glFrontFace(){}
+void glPointSize(){}
+void glLineWidth(){}
+void glLineStipple(){}
+void glPolygonMode(){}
+void glPolygonOffset(){}
+void glPolygonStipple(){}
+void glEdgeFlag(){}
+void glGetPolygonStipple(){}
+void glEdgeFlagv(){}
+void glScissor(){}
+void glClipPlane(){}
+void glGetClipPlane(){}
+void glDrawBuffer(){}
+void glReadBuffer(){}
+void glEnable(){}
+void glDisable(){} 
+void glIsEnabled(){}
+void glEnableClientState(){}
+void glDisableClientState(){}
+void glGetBooleanv(){}
+void glGetDoublev(){}
+void glGetFloatv(){}
+void glGetIntegerv(){}
+void glPopAttrib(){}
+void glPushClientAttrib(){}
+void glPopClientAttrib(){}
+void glRenderMode(){}
+void glGetError(){}
+void glGetString(){}
+void glFinish(){}
+void glFlush(){}
+void glClearDepth(){}
+void glDepthFunc(){}
+void glDepthMask(){}
+void glDepthRange(){}
+void glClearAccum(){}
+void glAccum(){}
+void glMatrixMode(){}
+void glOrtho(){}
+void glFrustum(){}
+void glViewport(){}
+void glPushMatrix(){}
+void glPopMatrix(){}
+void glLoadIdentity(){}
+void glLoadMatrixd(){}
+void glLoadMatrixf(){}
+void glMultMatrixd(){}
+void glMultMatrixf(){}
+void glRotated(){}
+void glRotatef(){}
+void glScaled(){}
+void glScalef(){}
+void glTranslated(){}
+void glTranslatef(){}
+void gllsList(){}
+void glDeleteLists(){}
+void glIsList(){}
+void glGenLists(){}
+void glNewList(){}
+void glEndList(){}
+void glCallList(){}
+void glCallLists(){}
+void glListBase(){}
+void glBegin(){}
+void glEnd(){}
+void glVertex2d(){}
+void glVertex2f(){}
+void glVertex2i(){}
+void glVertex2s(){}
+void glVertex3d(){}
+void glVertex3f(){}
+void glVertex3i(){}
+void glVertex3s(){}
+void glVertex4d(){}
+void glVertex4f(){}
+void glVertex4i(){}
+void glVertex4s(){}
+void glVertex2dv(){}
+void glVertex2fv(){}
+void glVertex2iv(){}
+void glVertex2sv(){}
+void glVertex3dv(){}
+void glVertex3fv(){}
+void glVertex3iv(){}
+void glVertex3sv(){}
+void glVertex4dv(){}
+void glVertex4fv(){}
+void glVertex4iv(){}
+void glVertex4sv(){}
+
+void glNormal3b(){}
+void glNormal3d(){}
+void glNormal3f(){}
+void glNormal3i(){}
+void glNormal3s(){}
+void glNormal3bv(){}
+void glNormal3dv(){}
+void glNormal3fv(){}
+void glNormal3iv(){}
+void glNormal3sv(){}
+void glIndexd(){}
+void glIndexf(){}
+void glIndexi(){}
+void glIndexs(){}
+void glIndexub(){}
+void glIndexdv(){}
+void glIndexfv(){}
+void glIndexiv(){}
+void glIndexsv(){}
+void glIndexubv(){}
+void glColor3b(){}
+void glColor3d(){}
+void glColor3f(){}
+void glColor3i(){}
+void glColor3s(){}
+void glColor3ub(){}
+void glColor3ui(){}
+void glColor3us(){}
+void glColor4b(){}
+void glColor4d(){}
+void glColor4f(){}
+void glColor4i(){}
+void glColor4s(){}
+void glColor4ub(){}
+void glColor4ui(){}
+void glColor4us(){}
+void glColor3bv(){}
+void glColor3dv(){}
+void glColor3fv(){}
+void glColor3iv(){}
+void glColor3sv(){}
+void glColor3ubv(){}
+void glColor3uiv(){}
+void glColor3usv(){}
+void glColor4bv(){}
+void glColor4dv(){}
+void glColor4fv(){}
+void glColor4iv(){}
+void glColor4sv(){}
+void glColor4ubv(){}
+void glColor4uiv(){}
+void glColor4usv(){}
+void glTexCoord1d(){}
+void glTexCoord1f(){}
+void glTexCoord1i(){}
+void glTexCoord1s(){}
+void glTexCoord2d(){}
+void glTexCoord2f(){}
+void glTexCoord2i(){}
+void glTexCoord2s(){}
+void glTexCoord3d(){}
+void glTexCoord3f(){}
+void glTexCoord3i(){}
+void glTexCoord3s(){}
+void glTexCoord4d(){}
+void glTexCoord4f(){}
+void glTexCoord4i(){}
+void glTexCoord4s(){}
+void glTexCoord1dv(){}
+void glTexCoord1fv(){}
+void glTexCoord1iv(){}
+void glTexCoord1sv(){}
+void glTexCoord2dv(){}
+void glTexCoord2fv(){}
+void glTexCoord2iv(){}
+void glTexCoord2sv(){}
+void glTexCoord3dv(){}
+void glTexCoord3fv(){}
+void glTexCoord3iv(){}
+void glTexCoord3sv(){}
+void glTexCoord4dv(){}
+void glTexCoord4fv(){}
+void glTexCoord4iv(){}
+void glTexCoord4sv(){}
+void glRasterPos2d(){}
+void glRasterPos2f(){}
+void glRasterPos2i(){}
+void glRasterPos2s(){}
+void glRasterPos3d(){}
+void glRasterPos3f(){}
+void glRasterPos3i(){}
+void glRasterPos3s(){}
+void glRasterPos4d(){}
+void glRasterPos4f(){}
+void glRasterPos4i(){}
+void glRasterPos4s(){}
+void glRasterPos2dv(){}
+void glRasterPos2fv(){}
+void glRasterPos2iv(){}
+void glRasterPos2sv(){}
+void glRasterPos3dv(){}
+void glRasterPos3fv(){}
+void glRasterPos3iv(){}
+void glRasterPos3sv(){}
+void glRasterPos4dv(){}
+void glRasterPos4fv(){}
+void glRasterPos4iv(){}
+void glRasterPos4sv(){}
+void glRectd(){}
+void glRectf(){}
+void glRecti(){}
+void glRects(){}
