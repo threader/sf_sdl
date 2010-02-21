0d9e298d75e23714d9e12a03e2bd561aa9eab889
bernd_afa <bernd_afa@d67ab018-90f1-4466-8658-4518960f82dd>
2010-02-21 09:08:07 +0000

    forget to upload new

--- video/SDL_pixels.c
+++ video/SDL_pixels.c
@@ -376,7 +376,7 @@ Uint32 SDL_MapRGBA
 		return SDL_FindColor(format->palette, r, g, b);
 	}}
 
-void SDL_GetRGBA(Uint32 pixel, SDL_PixelFormat *fmt,
+void SDL_GetRGBA(Uint32 pixel, const SDL_PixelFormat *fmt,
 		 Uint8 *r, Uint8 *g, Uint8 *b, Uint8 *a)
 {
 	if ( fmt->palette == NULL ) {
@@ -410,7 +410,7 @@ void SDL_GetRGBA(Uint32 pixel, SDL_PixelFormat *fmt,
 	}
 }
 
-void SDL_GetRGB(Uint32 pixel, SDL_PixelFormat *fmt, Uint8 *r,Uint8 *g,Uint8 *b)
+void SDL_GetRGB(Uint32 pixel, const SDL_PixelFormat *fmt, Uint8 *r,Uint8 *g,Uint8 *b)
 {
 	if ( fmt->palette == NULL ) {
 	        /* the note for SDL_GetRGBA above applies here too */
